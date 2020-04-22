cd40781f3ab9509ef6104bbc5e634ad77bdd4aef
kopniiin <kopniiin@gmail.com>
2020-04-22 10:09:45 +0300

    Добавляет переключение события и редактора события

--- src/main.js
+++ src/main.js
@@ -1,42 +1,49 @@
-import {compareDates} from "./utils";
+import {RenderPosition} from "./const";
 
-import {createInfoTemplate} from "./components/info";
-import {createMenuTemplate} from "./components/menu";
-import {createFilterTemplate} from "./components/filter";
-import {createSortTemplate} from "./components/sort";
-import {createEditorTemplate} from "./components/editor";
-import {createDayTemplate, createDayListTemplate} from "./components/day-list";
-import {createEventTemplate} from "./components/event";
+import {compareDates, render} from "./utils";
+
+import InfoComponent from "./components/info";
+import MenuComponent from "./components/menu";
+import FilterComponent from "./components/filter";
+import SortComponent from "./components/sort";
+import DayListComponent from "./components/day-list";
+import DayComponent from "./components/day";
+import EventComponent from "./components/event";
+import EditorComponent from "./components/editor";
 
 import {generateEvents} from "./mock/event";
 
-const render = (container, template, position = `beforeend`) => {
-  container.insertAdjacentHTML(position, template);
-};
-
 const mainElement = document.querySelector(`.trip-main`);
 const controlsElement = mainElement.querySelector(`.trip-controls`);
+const eventListElement = document.querySelector(`.trip-events`);
+const dayListElement = new DayListComponent().getElement();
 
-render(mainElement, createInfoTemplate(), `afterbegin`);
-render(controlsElement, createMenuTemplate());
-render(controlsElement, createFilterTemplate());
-
-const eventsElement = document.querySelector(`.trip-events`);
+render(mainElement, new InfoComponent().getElement(), RenderPosition.AFTERBEGIN);
+render(controlsElement, new MenuComponent().getElement());
+render(controlsElement, new FilterComponent().getElement());
+render(eventListElement, new SortComponent().getElement());
+render(eventListElement, dayListElement);
 
 const events = generateEvents();
 
-render(eventsElement, createSortTemplate());
-render(eventsElement, createEditorTemplate(events[0]));
-render(eventsElement, createDayListTemplate());
+const linkEventToEditor = (event, editor) => {
+  const eventElement = event.getElement();
+  const editorElement = editor.getElement();
+  const editButton = eventElement.querySelector(`.event__rollup-btn`);
 
-// Возможно, логика группировки событий по дням должна происходить внутри какого-либо компонента
-const dayListElement = eventsElement.querySelector(`.trip-days`);
+  const editButtonClickHandler = () => eventElement.replaceWith(editorElement);
+  const editorSubmitHandler = (evt) => {
+    evt.preventDefault();
+    editorElement.replaceWith(eventElement);
+  };
 
-let currentEventAmount = 1;
+  editButton.addEventListener(`click`, editButtonClickHandler);
+  editorElement.addEventListener(`submit`, editorSubmitHandler);
+};
 
-let currentDate = events[1].beginDate;
+let currentEventAmount = 0;
+let currentDate = events[0].beginDate;
 let eventsPerCurrentDate = 0;
-
 let dayCounter = 0;
 
 const renderDay = () => {
@@ -44,16 +51,23 @@ const renderDay = () => {
 
   const previousEventAmount = currentEventAmount;
   currentEventAmount += eventsPerCurrentDate;
-  eventsPerCurrentDate = 1;
 
-  const eventTemplates = events.slice(previousEventAmount, currentEventAmount)
-    .map(createEventTemplate);
+  const eventComponents = [];
 
-  render(dayListElement, createDayTemplate(dayCounter, currentDate, eventTemplates));
+  events.slice(previousEventAmount, currentEventAmount).map((event) => {
+    const eventComponent = new EventComponent(event);
+    linkEventToEditor(eventComponent, new EditorComponent(event));
+    eventComponents.push(eventComponent);
+  });
+
+  render(dayListElement, new DayComponent({
+    number: dayCounter,
+    date: currentDate,
+    events: eventComponents
+  }).getElement());
 };
 
-
-events.slice(currentEventAmount).forEach((event) => {
+events.forEach((event) => {
   const eventBeginDate = event.beginDate;
 
   if (compareDates(currentDate, eventBeginDate)) {
@@ -63,6 +77,7 @@ events.slice(currentEventAmount).forEach((event) => {
 
   renderDay();
   currentDate = eventBeginDate;
+  eventsPerCurrentDate = 1;
 });
 
 renderDay();
